d7a68e76d2d63b56e20154812d6d89cea613adab
Simon Watson <sw@simon-watson.com>
2022-09-19 21:24:37 +0800

    add access token renewal

--- SophosIntelix/Private/Get-SophosIntelixAccessHeader.ps1
+++ SophosIntelix/Private/Get-SophosIntelixAccessHeader.ps1
@@ -1,7 +1,16 @@
 function Get-SophosIntelixAccessHeader {
-    $access = @{
-        Authorization      = $SCRIPT:SophosIntelixAuth['AccessToken']
-        'X-Correlation-ID' = (New-Guid).Guid
+
+    if ($null -ne $SCRIPT:SophosIntelixAuth['AccessToken']) {
+        if ($SCRIPT:SophosIntelixAuth.ExpiresAt -le (Get-Date)) {
+            Connect-SophosIntelix -ClientID $SCRIPT:SophosIntelixAuth.ClientID -ClientSecret $SCRIPT:SophosIntelixAuth.ClientSecret
+        }
+
+        $access = @{
+            Authorization      = $SCRIPT:SophosIntelixAuth['AccessToken']
+            'X-Correlation-ID' = (New-Guid).Guid
+        }
+        $access
+    } else {
+        throw 'Login using Connect-SophosIntelix'
     }
-    $access
 }
--- SophosIntelix/Private/Invoke-SophosIntelixWebRequest.ps1
+++ SophosIntelix/Private/Invoke-SophosIntelixWebRequest.ps1
@@ -5,5 +5,7 @@ function Invoke-SophosIntelixWebRequest {
     )
     $uri = 'https://' + $SCRIPT:SophosIntelixAuth['DefaultRegion'] + '.api.labs.sophos.com/' + $UriEndpoint
     $accessHeader = Get-SophosIntelixAccessHeader
-    Invoke-RestMethod -Uri $uri -Headers $accessHeader -Verbose:$Verbose
+    if ($null -ne $accessHeader) {
+        Invoke-RestMethod -Uri $uri -Headers $accessHeader -Verbose:$Verbose
+    }
 }
