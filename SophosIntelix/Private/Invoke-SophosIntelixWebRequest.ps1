03c0373b5b0141fd2b3b049ae15139f4be98f3be
Simon Watson <sw@simon-watson.com>
2022-09-22 10:15:16 +0800

    initial post support

--- SophosIntelix/Private/Invoke-SophosIntelixWebRequest.ps1
+++ SophosIntelix/Private/Invoke-SophosIntelixWebRequest.ps1
@@ -1,11 +1,17 @@
 function Invoke-SophosIntelixWebRequest {
     [CmdletBinding()]
     param (
-        [string]$UriEndpoint
+        [string]$UriEndpoint,
+        [string]$Method = 'Get',
+        [hashtable]$Body
     )
     $uri = 'https://' + $SCRIPT:SophosIntelixAuth['DefaultRegion'] + '.api.labs.sophos.com/' + $UriEndpoint
     $accessHeader = Get-SophosIntelixAccessHeader
     if ($null -ne $accessHeader) {
-        Invoke-RestMethod -Uri $uri -Headers $accessHeader -Verbose:$Verbose
+        if ($Method -eq 'Get') {
+            Invoke-RestMethod -Uri $uri -Headers $accessHeader
+        } elseif ($Method -eq 'Post') {
+            Invoke-RestMethod -Uri $uri -Headers $accessHeader -Method Post -Body $Body
+        }
     }
 }
